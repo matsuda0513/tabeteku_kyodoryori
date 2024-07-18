62720b2dc44e9392c6cf0f9c36931f250841b6b2
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-18 17:30:00 +0900

    県名検索の追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,11 +5,14 @@ class Food < ApplicationRecord
   validates :image_url, presence: true
   validates :detail_url, presence: true, uniqueness: true
 
-  def self.search(search)
-    if search != ""
-      Food.where('text LIKE(?)', "%#{search}%")
-    else
-      Food.all
+  def self.search(name_keyword, prefecture_keyword)
+    foods = Food.all
+    if name_keyword.present?
+      foods = foods.where('name LIKE ?', "%#{name_keyword}%")
     end
+    if prefecture_keyword.present?
+      foods = foods.where('prefecture LIKE ?', "%#{prefecture_keyword}%")
+    end
+    foods
   end
 end
