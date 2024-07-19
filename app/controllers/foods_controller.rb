9514a5e648761fd47c92e69454f51f5c51b5d3ea
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-19 11:19:42 +0900

    食べログ検索機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,6 +5,9 @@ class FoodsController < ApplicationController
 
   def show
     @food = Food.find(params[:id])
+    encoded_name = URI.encode_www_form_component(@food.name)
+    encoded_prefecture = URI.encode_www_form_component(@food.prefecture)
+
     @kyodoryouri_info = {
       name: @food.name,
       prefecture: @food.prefecture,
@@ -12,9 +15,11 @@ class FoodsController < ApplicationController
       image_url: @food.image_url,
       image_source: @food.image_credit
     }
+      @tabelog_url = "https://tabelog.com/rstLst/?vs=1&sa=&sk=#{encoded_name}&sw=#{encoded_name}&trailing_slash=true&srchTg=2"
+      # https://tabelog.com/rst/rstsearch/?LstKind=1&voluntary_search=1&lid=top_navi1&sk=#{encoded_name}&sa_input=#{encoded_prefecture}
   end
-
+  
   def search
-    @foods = Food.search(params[:keyword])
+    @foods = Food.search(params[:name_keyword], params[:prefecture_keyword])
   end
 end
