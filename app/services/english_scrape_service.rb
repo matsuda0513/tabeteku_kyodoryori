43bcd5c95c0de2c2a19deb13b9117d016d056cbf
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-25 12:17:42 +0900

    英語化

--- app/services/english_scrape_service.rb
+++ app/services/english_scrape_service.rb
@@ -4,11 +4,11 @@ require 'open-uri'
 
 class EnglishScrapeService
   BASE_URL = 'https://www.maff.go.jp/e/policies/market/k_ryouri/search_menu/syllabary/index.html'  # 英語版のスクレイピング対象URL
-  DETAIL_BASE_URL = 'https://www.maff.go.jp/e/policies/market/k_ryouri/search_menu/'
+  DETAIL_BASE_URL = 'https://www.maff.go.jp/e/policies/market/k_ryouri/search_menu/menu/'
 
   def self.fetch_all_english_kyodoryouri_links
     options = Selenium::WebDriver::Chrome::Options.new
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
 
     driver = Selenium::WebDriver.for :chrome, options: options
     driver.get(BASE_URL)
@@ -17,14 +17,19 @@ class EnglishScrapeService
     doc = Nokogiri::HTML(driver.page_source)
     driver.quit
 
-    search_menu_top = doc.at('.inner')
-    links = search_menu_top ? search_menu_top.css('.menu_details .tit06 a').map { |link| { name: link.text.strip, url: DETAIL_BASE_URL + link['href'] } } : []
+    links = []
+    doc.css('ul li.list-group-item a').each do |link|
+      href = link['href']
+      full_url = URI.join(DETAIL_BASE_URL, href).to_s
+      links << { name: link.text.strip, url: full_url }
+    end
+
     links
   end
 
   def self.fetch_english_kyodoryouri_details(url)
     options = Selenium::WebDriver::Chrome::Options.new
-    # options.add_argument('--headless')  # ヘッドレスモードで実行するオプションを追加
+    options.add_argument('--headless')  # ヘッドレスモードで実行するオプションを追加
 
     driver = Selenium::WebDriver.for :chrome, options: options
     driver.get(url)
@@ -33,21 +38,20 @@ class EnglishScrapeService
     doc = Nokogiri::HTML(driver.page_source)
     driver.quit
 
-    name = doc.at('.menu_details .tit06 span.name').text.strip
-    prefecture = doc.at('.menu_details .tit06 .pref').text.strip
-    history = doc.at('ul.menu_details li h3:contains("History/origin/related events") + p').text.strip
+    name = doc.at('.menu_details .tit06 span.name')&.text&.strip || ''
+    prefecture = doc.at('.menu_details .tit06 .pref')&.text&.strip || ''
+    history = doc.at('ul.menu_details li h3:contains("History/origin/related events") + p')&.text&.strip || ''
     image_element = doc.at('.menu_main .resp_img')
-    image_url = URI.join(url, image_element['src']).to_s
+    image_url = URI.join(url, image_element['src']).to_s if image_element
+
     image_elements = doc.css('.thumb02 .js_modal01 img.resp_img')
-    first_image_src = image_elements.first['src'] if image_elements.any?
+    first_image_element = image_elements.first
     image_source = nil
 
-    image_elements.each do |element|
-      if element['src'] == first_image_src
-        image_source_element = element.parent.next_element.css('.print_none').text.strip
-        image_source = image_source_element if image_source_element
-        break
-      end
+    if first_image_element
+      parent_div = first_image_element.parent
+      image_source_element = parent_div.next_element.at('p')rescue nil
+      image_source = image_source_element&.text&.strip
     end
 
     {
@@ -58,6 +62,17 @@ class EnglishScrapeService
       image_source: image_source,
       detail_url: url
     }
+  rescue => e
+    puts "Error fetching details for URL: #{url}"
+    puts e.message
+    {
+      name: '',
+      prefecture: '',
+      history: '',
+      image_url: '',
+      image_source: '',
+      detail_url: url
+    }
   end
 
   def self.fetch_and_save_all_english_kyodoryouri
@@ -65,14 +80,18 @@ class EnglishScrapeService
     links.each do |link|
       details = fetch_english_kyodoryouri_details(link[:url])
       food = EnglishFood.find_or_initialize_by(detail_url: details[:detail_url])
-      food.update!(
-        name: details[:name],
-        prefecture: details[:prefecture],
-        history: details[:history],
-        image_url: details[:image_url],
-        image_credit: details[:image_source],
-        detail_url: details[:detail_url]
-      )
+      if food.new_record? || food.name != details[:name] || food.prefecture != details[:prefecture] || 
+        food.history != details[:history] || food.image_url != details[:image_url] || food.image_credit != details[:image_source]
+       
+       food.update!(
+         name: details[:name],
+         prefecture: details[:prefecture],
+         history: details[:history],
+         image_url: details[:image_url],
+         image_credit: details[:image_source],
+         detail_url: details[:detail_url]
+       )
     end
   end
+end
 end
